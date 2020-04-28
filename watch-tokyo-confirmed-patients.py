ab79f357c16053aa96706d07d8784d12ff62d68f
Kunihiko MIYOSHI <miyoshik@gmail.com>
2020-04-28 12:19:52 +0900

    fix legend

--- watch-tokyo-confirmed-patients.py
+++ watch-tokyo-confirmed-patients.py
@@ -19,18 +19,22 @@ df_tokyo_log
 
 
 # %%
-df_by_gender = df_tokyo_log.groupby(['公表_年月日', '患者_性別']).count()['都道府県名'].reset_index()
-df_by_gender = df_by_gender.rename(columns={'公表_年月日':'date', '患者_性別':'gender', '都道府県名':'count'})
+df_by_gender = df_tokyo_log.groupby(['公表_年月日', '患者_性別']).count()[
+    '都道府県名'].reset_index()
+df_by_gender = df_by_gender.rename(
+    columns={'公表_年月日': 'date', '患者_性別': 'gender', '都道府県名': 'count'})
 df_by_gender = df_by_gender.pivot_table('count', 'date', 'gender').fillna(0)
-df_by_gender = df_by_gender.rename(columns={'男性':'M', '女性':'F'})
+df_by_gender = df_by_gender.rename(columns={'男性': 'M', '女性': 'F'})
 df_by_gender
 
 
 # %%
 window = 7
-title = "Daily confirmed number of positive patients by gender ({} day rolling ave)".format(window)
-ax = df_by_gender[['M','F']].rolling(window).mean().plot(title=title, grid=True, cmap=plt.get_cmap("tab10"))
-ax.legend(bbox_to_anchor=(1, 1.03), loc='upper left')
+title = "Daily confirmed number of positive patients by gender ({} day rolling ave)".format(
+    window)
+ax = df_by_gender[['M', 'F']].rolling(window).mean().plot(
+    title=title, grid=True, cmap=plt.get_cmap("tab10"))
+ax.legend(bbox_to_anchor=(0.05, 1), loc='upper left')
 ax.xaxis.set_major_locator(mdates.MonthLocator())
 
 reference_dates = pd.to_datetime([
@@ -38,7 +42,7 @@ reference_dates = pd.to_datetime([
     '2020-03-03',
     '2020-03-31',
     '2020-04-19'
-    ])
+])
 plt.ylabel("Confirmed number of positive patients")
 plt.vlines(reference_dates, 0, 100, "red", linestyles='dashed')
 plt.savefig(os.path.join('plots', title))
@@ -46,8 +50,10 @@ plt.savefig(os.path.join('plots', title))
 
 
 # %%
-df_by_age = df_tokyo_log.groupby(['公表_年月日', '患者_年代']).count()['都道府県名'].reset_index()
-df_by_age = df_by_age.rename(columns={'公表_年月日':'date', '患者_年代':'age', '都道府県名':'count'})
+df_by_age = df_tokyo_log.groupby(['公表_年月日', '患者_年代']).count()[
+    '都道府県名'].reset_index()
+df_by_age = df_by_age.rename(
+    columns={'公表_年月日': 'date', '患者_年代': 'age', '都道府県名': 'count'})
 df_by_age = df_by_age.pivot_table('count', 'date', 'age').fillna(0)
 df_by_age = df_by_age.rename(columns={
     '10代': '15',
@@ -60,16 +66,19 @@ df_by_age = df_by_age.rename(columns={
     '80代': '80',
     '90代': '90',
     '100歳以上': '100',
-    })
+})
 # df_by_age['70'] = df_by_age.apply(lambda x: x['70'] + x['80'] + x['90'] + x['100'], axis=1)
-df_by_age = df_by_age.drop(columns=['-', '10歳未満','不明', '80', '90', '100'], axis=1)
+df_by_age = df_by_age.drop(
+    columns=['-', '10歳未満', '不明', '80', '90', '100'], axis=1)
 df_by_age
 
 
 # %%
-title = "Daily confirmed number of positive patients by age ({} day rolling ave)".format(window)
-ax = df_by_age.rolling(window).mean().plot(title=title, grid=True, cmap=plt.get_cmap("tab10"))
-ax.legend(bbox_to_anchor=(1, 1.03), loc='upper left')
+title = "Daily confirmed number of positive patients by age ({} day rolling ave)".format(
+    window)
+ax = df_by_age.rolling(window).mean().plot(
+    title=title, grid=True, cmap=plt.get_cmap("tab10"))
+ax.legend(bbox_to_anchor=(0.05, 1), loc='upper left')
 ax.xaxis.set_major_locator(mdates.MonthLocator())
 
 plt.ylabel("Confirmed number of positive patients")
@@ -82,4 +91,3 @@ plt.savefig(os.path.join('plots', title))
 # TODO calcurate positive ratio by age class
 # https://www.toukei.metro.tokyo.lg.jp/dyosoku/dy-data.htm
 # https://www.toukei.metro.tokyo.lg.jp/dyosoku/dy17ra0901.xls
-
